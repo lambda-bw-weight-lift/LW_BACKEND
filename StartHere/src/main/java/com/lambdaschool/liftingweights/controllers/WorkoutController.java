4c424f6f8e2d5481ccee16b4e4ff171b1d0d275e
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-09-23 21:20:08 -0700

    finished workout and exercise controllers - aka endpoints.

--- StartHere/src/main/java/com/lambdaschool/liftingweights/controllers/WorkoutController.java
+++ StartHere/src/main/java/com/lambdaschool/liftingweights/controllers/WorkoutController.java
@@ -2,21 +2,24 @@ package com.lambdaschool.liftingweights.controllers;
 
 import com.lambdaschool.liftingweights.models.ErrorDetail;
 import com.lambdaschool.liftingweights.models.Exercise;
+import com.lambdaschool.liftingweights.models.User;
 import com.lambdaschool.liftingweights.models.UserWorkout;
 import com.lambdaschool.liftingweights.services.ExerciseService;
 import com.lambdaschool.liftingweights.services.UserWorkoutService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/workouts")
@@ -30,28 +33,70 @@ public class WorkoutController {
     @Autowired
     UserWorkoutService workoutService;
 
-    //    https://weight-lifting-journal-bw.herokuapp.com/
-    //    1. GET workouts/{userid}/previous - lists all previous workouts
+    //  BASE URL: https://weight-lifting-journal-bw.herokuapp.com/
+    //  GET /workouts/all - lists all previous workouts
+    @ApiOperation(value = "Returns all previous workouts with ability for paging and sorting.", response = UserWorkout.class, responseContainer = "List")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", dataType = "integer", paramType = "query",
+                    value = "Results page you want to retrieve (0..N)"),
+            @ApiImplicitParam(name = "size", dataType = "integer", paramType = "query",
+                    value = "Number of records per page."),
+            @ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query",
+                    value = "Sorting criteria in the format: property(,asc|desc). " +
+                            "Default sort order is ascending. " +
+                            "Multiple sort criteria are supported.")})
+    @ApiResponses({
+            @ApiResponse(code = 200, response = UserWorkout.class, responseContainer = "List", message = "It worked!")
+    })
+    @GetMapping(value = "/all", produces = {"application/json"})
+    public ResponseEntity<?> getAllWorkouts(@PageableDefault(page = 0, size = 5)Pageable pageable) {
+        List<UserWorkout> allWorkouts = new ArrayList<>();
+        workoutService.findAll(pageable).iterator().forEachRemaining(allWorkouts::add);
+        return new ResponseEntity<>(allWorkouts, HttpStatus.OK);
+    }
+
+    // GET /workouts/{workoutid} - lists all exercises for a specific workout
+    @ApiOperation(value = "Lists all exercises for a specific workout.")
+    @ApiResponses({
+            @ApiResponse(code = 200, response = UserWorkout.class, responseContainer = "List", message = "It worked!")
+    })
+    @GetMapping(value = "/{workoutid}", produces = {"application/json"})
+    public ResponseEntity<?> getAllExercisesByWorkoutId(@PathVariable long workoutid) {
+        UserWorkout workout = workoutService.findById(workoutid);
+
+        List<Exercise> allExercises = new ArrayList<>();
+
+        return new ResponseEntity<>(allExercises, HttpStatus.OK);
+    }
 
 
-    //    2. GET workouts/{userid}/previous/{date}/{workoutid} - lists all exercises for a specific past workout
+    // POST /workouts/current - create a new workout 201 & 500
+    @ApiOperation(value = "Creates a new Workout", response = UserWorkout.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, response = UserWorkout.class, message = "It worked!"),
+            @ApiResponse(code = 500, message = "Error creating a new workout.", response = ErrorDetail.class)
+    })
+    @PostMapping(value = "/current", produces = {"application/json"})
+    public ResponseEntity<?> createNewWorkout(@Valid @RequestBody UserWorkout newWorkout) {
+        UserWorkout workout = workoutService.saveWorkout(newWorkout);
+        return new ResponseEntity<>(workout, HttpStatus.CREATED);
+    }
 
 
-    //    3. POST workouts/{userid}/exercise/{date} - add a new exercise to the current workout
+    // POST /workouts/{workoutid} - add a new exercise to the workout
     @ApiOperation(value = "updates workout with a new exercise", response = UserWorkout.class, responseContainer = "List")
     @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Exercise successfully added to existing workout", response = void.class),
+            @ApiResponse(code = 202, message = "Exercise successfully added to existing workout", response = void.class),
             @ApiResponse(code = 500, message = "Error updating workout exercise", response = ErrorDetail.class)
     })
-    @PostMapping(value = "/workouts/{userid}/exercise/{workoutid}", consumes = {"application/json"}, produces = {"application/json"})
-    public ResponseEntity<?> updateBookAuthor(@Valid
-                                              @RequestBody Exercise exercise,
-                                              @PathVariable
-                                                      long userid, @PathVariable long workoutid, HttpServletRequest request) {
+    @PostMapping(value = "/workouts/{workoutid}", produces = {"application/json"})
+    public ResponseEntity<?> updateWorkoutExercise(@Valid
+                                              @RequestBody Exercise updateExercise,
+                                              @PathVariable long workoutid, HttpServletRequest request) {
         logger.trace(request.getMethod().toUpperCase() + " " + request.getRequestURI() + " accessed.");
 
-        workoutService.saveExerciseToWorkout(workoutid, exercise);
-        return new ResponseEntity<>(HttpStatus.OK);
+        workoutService.saveExerciseToWorkout(workoutid, updateExercise);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
 }
--- StartHere/src/main/java/com/lambdaschool/liftingweights/services/UserWorkoutServiceImpl.java
+++ StartHere/src/main/java/com/lambdaschool/liftingweights/services/UserWorkoutServiceImpl.java
@@ -2,6 +2,7 @@ package com.lambdaschool.liftingweights.services;
 
 import com.lambdaschool.liftingweights.models.Exercise;
 import com.lambdaschool.liftingweights.models.UserWorkout;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 @Service(value = "userWorkoutService")
 public class UserWorkoutServiceImpl implements UserWorkoutService {
     @Override
-    public List<UserWorkout> findAll() {
+    public List<UserWorkout> findAll(Pageable pageable) {
         return null;
     }
 
@@ -19,8 +20,18 @@ public class UserWorkoutServiceImpl implements UserWorkoutService {
     }
 
     @Override
+    public UserWorkout findById(long id) {
+        return null;
+    }
+
+    @Override
     public void delete(long workoutid) {
 
+    }
+
+    @Override
+    public UserWorkout saveWorkout(UserWorkout workout) {
+        return null;
     }
 
     @Override
