7a47c7ba977d8bd6209728737e3251397fd7f48a
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-09-26 12:38:13 -0700

    changes I do not want.

---
+++ StartHere/src/test/java/com/lambdaschool/liftingweights/StartHereApplicationTest.java
@@ -0,0 +1,21 @@
+package com.lambdaschool.liftingweights;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StartHereApplicationTest {
+
+    @Test
+    public void contextLoads()
+    {
+
+    }
+
+
+}
